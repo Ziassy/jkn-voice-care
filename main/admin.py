354f96b70c0aa9889289f1e82b40c534810bb5f9
ziassy <fauziahfz216@gmail.com>
2023-09-10 23:41:55 +0700

    [ADD] Models and update html page for integration

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,18 @@
-# main/admin.py
 from django.contrib import admin
-from .models import LanguageChoice
+from .models import LanguageChoice, TranslatedMenu, Translation
 
 
-class LanguageChoicesAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name']
+@admin.register(LanguageChoice)
+class LanguageChoiceAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
 
 
-admin.site.register(LanguageChoice, LanguageChoicesAdmin)
+@admin.register(TranslatedMenu)
+class TranslatedMenuAdmin(admin.ModelAdmin):
+    list_display = ('menu_id', 'menu_name')
+
+
+@admin.register(Translation)
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ('menu', 'language', 'translation')
+    list_filter = ('menu', 'language')
--- main/models.py
+++ main/models.py
@@ -1,12 +1,25 @@
-# models.py
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class LanguageChoice(models.Model):
     code = models.CharField(max_length=5, unique=True)
     name = models.CharField(max_length=50)
+    translation_enabled = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+class TranslatedMenu(models.Model):
+    menu_id = models.CharField(max_length=50, unique=True)
+    menu_name = models.CharField(max_length=50)
+    translations = models.ManyToManyField(LanguageChoice, through='Translation')
+
+    def __str__(self):
+        return self.menu_name
+
+class Translation(models.Model):
+    menu = models.ForeignKey(TranslatedMenu, on_delete=models.CASCADE)
+    language = models.ForeignKey(LanguageChoice, on_delete=models.CASCADE)
+    translation = models.TextField()
+
+    def __str__(self):
+        return f"{self.menu.menu_name} ({self.language.name})"
