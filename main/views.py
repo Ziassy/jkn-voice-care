65178b2c0c989cb70975362a54b1d9a6abd7c1af
ziassy <fauziahfz216@gmail.com>
2023-09-12 20:48:46 +0700

    [CHANGE] position submenu in html and unfinish query submenu

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from gtts import gTTS
 import os
 from django.conf import settings
-from .models import LanguageChoice, TranslatedMenu, Translation
+from .models import LanguageChoice, TranslatedMenu, Translation, SubMenu
 
 
 def translate(request):
@@ -19,10 +19,15 @@ def translate(request):
 
     menus = TranslatedMenu.objects.all()
 
+    menu_submenus = {}
+
    # translation based on the menu ID
     for menu in menus:
         translation = translations.filter(menu__menu_id=menu.menu_id).first()
         menu_translations[menu.menu_name] = translation.translation if translation else 'Translation not available'
+
+        submenus = SubMenu.objects.filter(menu_id=menu.id)
+        menu_submenus[menu.menu_name] = submenus
 
     if request.method == 'GET':
         for menu_id, translation_text in menu_translations.items():
@@ -38,12 +43,13 @@ def translate(request):
                 audio_path = os.path.join(settings.MEDIA_ROOT, audio_filename)
                 if os.path.exists(audio_path):
                     os.remove(audio_path)
+        print(menu_submenus)
         print(menu_translations)
 
     else:
         print("Tidak tergenerate")
 
-    return render(request, 'main/template.html', {'languages': languages, 'selected_language': selected_language, 'selected_code': selected_code, 'menus': menus, 'menu_translations': menu_translations})
+    return render(request, 'main/template.html', {'languages': languages, 'selected_language': selected_language, 'selected_code': selected_code, 'menus': menus, 'menu_translations': menu_translations, 'menu_submenus': menu_submenus})
 
 
 def menu_detail(request, menu_id):
