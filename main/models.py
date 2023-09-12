1f3263e6918fbe828ba5bc96d4e82cf5503c9283
ziassy <fauziahfz216@gmail.com>
2023-09-12 12:47:39 +0700

    [ADD] Model for submenu and subtranslation

--- main/models.py
+++ main/models.py
@@ -27,3 +27,27 @@ class Translation(models.Model):
 
     def __str__(self):
         return f"{self.menu.menu_name} ({self.language.name})"
+    
+class SubMenu(models.Model):
+    submenu_name = models.CharField(max_length=50)
+    translation = models.ManyToManyField(
+        LanguageChoice, through='SubTranslation')
+    image_url = models.URLField(
+        default='https://s3.us-east-1.amazonaws.com/hyunday-file/1694438095296-Avatar.png')
+    detail_url = models.URLField(default="")
+    menu_id = models.ForeignKey(
+        TranslatedMenu, on_delete=models.CASCADE, related_name='submenus')
+
+    def __str__(self):
+        return self.submenu_name
+
+class SubTranslation(models.Model):
+    submenu = models.ForeignKey(SubMenu, on_delete=models.CASCADE)
+    language = models.ForeignKey(LanguageChoice, on_delete=models.CASCADE)
+    translation = models.TextField()
+
+    def __str__(self):
+        return f"{self.submenu.submenu_name} ({self.language.name})"
+
+
+
