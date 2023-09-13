ab6e507cdfabd7b1d1b8f845c1592e4f2713e63f
ziassy <fauziahfz216@gmail.com>
2023-09-13 17:08:52 +0700

    [DELETE] Unused code from views.py

--- main/views.py
+++ main/views.py
@@ -19,36 +19,21 @@ def translate(request):
     submenu_translations = {}
 
     menus = TranslatedMenu.objects.all()
-    selected_menu_id = '001'
-
-    menu_submenus = {}
 
     # translation based on the menu ID
     for menu in menus:
         translation = translations.filter(menu__menu_id=menu.menu_id).first()
         menu_translations[menu.menu_name] = translation.translation if translation else 'Translation not available'
 
-        # Retrieve submenus for the current menu
-        submenus = SubMenu.objects.filter(menu_id=menu.id)
-
-        # Convert submenus to a list of dictionaries
-        submenus_list = [{'submenu_name': submenu.submenu_name}
-                         for submenu in submenus]
-
-        # Store the list of submenus in the menu_submenus dictionary
-        menu_submenus[menu.menu_id] = submenus_list
-
+        # Filter submenus for the current menu
+        submenus = SubMenu.objects.filter(menu_id=menu.menu_id)
         # Translation for submenus based on the submenu ID
-        for menu in menus:
-            # Filter submenus for the current menu
-            submenus = SubMenu.objects.filter(menu_id=menu.menu_id)
-
-            for submenu in submenus:
-                submenu_translation = SubTranslation.objects.filter(
-                    submenu=submenu,
-                    language__code=selected_language
-                ).first()
-                submenu_translations[submenu.submenu_name] = submenu_translation.translation if submenu_translation else 'Translation not available'
+        for submenu in submenus:
+            submenu_translation = SubTranslation.objects.filter(
+                submenu=submenu,
+                language__code=selected_language
+            ).first()
+            submenu_translations[submenu.submenu_name] = submenu_translation.translation if submenu_translation else 'Translation not available'
 
     if request.method == 'GET':
         for menu_id, translation_text in menu_translations.items():
@@ -67,14 +52,13 @@ def translate(request):
 
         for submenu_id, submenu_translation_text in submenu_translations.items():
             if submenu_translation_text != 'Translation not available':
-                # Use submenu ID as the audio file name for submenus
-                audio_filename = f'audio-submenu-{submenu_id}.mp3'
+                audio_filename = f'audio-{submenu_id}.mp3'
                 tts = gTTS(text=submenu_translation_text, lang='id')
                 audio_path = os.path.join(settings.MEDIA_ROOT, audio_filename)
                 tts.save(audio_path)
             else:
                 # Delete audio file if 'Translation not available'
-                audio_filename = f'audio-submenu-{submenu_id}.mp3'
+                audio_filename = f'audio-{submenu_id}.mp3'
                 audio_path = os.path.join(settings.MEDIA_ROOT, audio_filename)
                 if os.path.exists(audio_path):
                     os.remove(audio_path)
@@ -89,8 +73,6 @@ def translate(request):
         'selected_code': selected_code,
         'menus': menus,
         'menu_translations': menu_translations,
-        'menu_submenus': menu_submenus,
-        'selected_menu_id': selected_menu_id
     })
 
 
