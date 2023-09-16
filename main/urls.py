a38a30bac63893391cf3a6ac6a981f4dcc764f7a
ziassy <fauziahfz216@gmail.com>
2023-09-16 23:19:11 +0700

    [ADD] Page not found for detail submenu

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.contrib import admin
+from django.views.defaults import page_not_found
 
 urlpatterns = [
     path('', views.translate, name='translate'),
     path('submenu/<str:detail_url>/', views.menu_detail, name='submenu_detail'),
+    re_path(r'^notfound/$', lambda request,
+            exception=None: page_not_found(request, exception), name='notfound'),
 ]
 
 if settings.DEBUG:
