2b17d56cdadeafc82fdcd824ff0698b2fcf424ac
ExRufus <akmaldyn02@gmail.com>
2023-09-17 10:46:39 +0700

    test

---
+++ main/static/js/pageutama.js
@@ -0,0 +1,56 @@
+const customButtons = document.getElementsByClassName("custom-button");
+const toggleCheckbox = document.getElementById("toogleCheckbox");
+
+function showCustomButton() {
+const isActive = toggleCheckbox.checked;
+
+for (let i = 0; i < customButtons.length; i++) {
+  customButtons[i].style.display = isActive ? 'block' : 'none';
+}
+
+const menuIcons = document.querySelectorAll(".menu-icon");
+
+menuIcons.forEach((menuIcon) => {
+  if (isActive) {
+    menuIcon.style.display = 'block';
+    buttonPelayanan.addEventListener('click', showPopup);
+  } else {
+    menuIcon.style.display = 'none';
+  }
+});
+}
+
+  toggleCheckbox.addEventListener('change', showCustomButton);
+  showCustomButton();
+
+  // popup
+  const popupantrean = document.getElementById('popupantrean');
+  const buttonPelayanan = document.getElementById('button-pelayanan');
+
+
+  // Function to show the popup
+  function showPopup() {
+    popupantrean.style.display = 'block';
+    body.style.backgroundColor = 'rgb(46, 43, 43)';
+  }
+
+
+  /* When the user clicks on the button, 
+  toggle between hiding and showing the dropdown content */
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
